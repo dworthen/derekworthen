0c8a5028c4c2817d873e898f25efb997b2a51f27
Derek Worthen <worthend.derek@gmail.com>
2011-10-08 00:00:29 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var http = require('http'),
+		fs   = require('fs'),
+		path = require('path');
+
+http.createServer(function(req, res) {
+	var filepath = req.url,
+			contentType,
+			extname;
+
+	if (filepath == '/') {
+		filepath = '/index.html';
+	}
+
+	filepath = '.' + filepath;
+
+	extname = path.extname(filepath);
+
+	switch(extname) {
+		case '.js' :
+			contentType = 'text/javascript';
+		break;
+		case '.css' :
+			contentType = 'text/css';
+		break;
+		default:
+			contentType = 'text/html';
+	}
+
+	path.exists(filepath, function (exists) {
+		if (exists) {
+			fs.readFile(filepath, function (err, content) {
+				if (err) {
+					res.writeHead(500);
+					res.end(err);	
+				} else {
+					res.writeHead(200, {'content-type': contentType});
+					res.end(content, 'utf-8');
+				}
+			});	
+		} else {
+			res.writeHead(400);
+			res.end('404 - Page Not Found');
+		}
+	});
+}).listen(3000);
